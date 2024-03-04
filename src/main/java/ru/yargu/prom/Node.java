a6ac2b7a709786679007243a10efed67606ac3c3
Irina-Tolmach <tolma4.irina@gmail.com>
2024-03-04 17:00:48 +0300

    first

---
+++ src/main/java/ru/yargu/prom/Node.java
@@ -0,0 +1,41 @@
+package ru.yargu.prom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    Integer ID;
+    String name;
+    List<Node> children = new ArrayList();
+    public Node(String name) {
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public void addSon(Node son) {
+    }
+
+    public void deleteSon(String name) {
+
+    }
+
+    public void deleteID(Integer ID) {
+    }
+
+    public Integer getID() {
+        return null;
+    }
+
+    public void deleteAll() {
+    }
+
+    public Node find(String name) {
+
+        return null;
+    }
+
+    public void setName(String stepFather) {
+    }
+}
---
+++ src/test/java/ru/yargu/prom/TestNode.java
@@ -0,0 +1,92 @@
+package ru.yargu.prom;
+
+import org.junit.jupiter.api.*;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestNode {
+
+    /**
+     * Проверка создания дерева
+     */
+    @Test
+    void Test1CreateNewTree(){
+        Node node = new Node("node1");
+        assertNotNull(node, "Узел не создан");
+        assertEquals("node1", node.getName());
+    }
+
+    /**
+     * Добавление дочернего узла
+     */
+    @Test
+    void Test2AddNewNode(){
+        Node node1 = new Node("father");
+        Node node2 = new Node("son");
+        node1.addSon(node2);
+        assertFalse(node1.children.isEmpty());
+        assertTrue(node1.children.contains(node2));
+    }
+
+    /**
+     * Удаление дочернего узла по имени
+     */
+    @Test
+    void Test3DeleteNodeByName(){
+        Node node1 = new Node("father");
+        Node node2 = new Node("son");
+        assertDoesNotThrow(() -> node1.addSon(node2));
+        node1.deleteSon(node2.getName());
+        assertTrue(node1.children.isEmpty());
+    }
+
+    /**
+     * Удаление дочернего узла по идентификатору
+     */
+    @Test
+    void Test4DeleteNodeByID(){
+        Node node1 = new Node("father");
+        Node node2 = new Node("son");
+        assertDoesNotThrow(() -> node1.addSon(node2));
+
+        node1.deleteID(node2.getID());
+        assertEquals(node1.children.isEmpty(), true);
+    }
+
+    /**
+     * Удаление всех дочерних узлов
+     */
+    @Test
+    void Test5DeleteAllDaugtherNode(){
+        Node node1 = new Node("father");
+        Node node2 = new Node("son");
+        Node node3 = new Node("son2");
+        node1.addSon(node2);
+        node1.addSon(node3);
+
+        node1.deleteAll();
+        assertTrue(node1.children.isEmpty());
+    }
+
+    /**
+     * Нахождение узлов по имени
+     */
+    @Test
+    void Test6FindNodeByName(){
+        Node node1 = new Node("father");
+        Node node2 = new Node("son");
+        assertEquals(node1.find(node2.getName()), node2);
+    }
+
+    /**
+     * Изменение значения узла
+     */
+    @Test
+    void Test7RewriteNode(){
+        Node node1 = new Node("father");
+        node1.setName("stepFather");
+        assertEquals(node1.getName(), "stepFather");
+    }
+
+}
